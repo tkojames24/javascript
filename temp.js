511740cfb144d2cc824882dcd9df31709fa5f717
tkojames <ejmathison@ucdavis.edu>
2016-04-26 22:30:21 -0700

    temp.js

---
+++ temp.js
@@ -0,0 +1,17 @@
+
+//Get the input from user
+var a = prompt("Enter a temp in Fahrenheit");
+//Make the input a variable 
+var b;
+//convert the number to C and round whole numbers
+b = Math.round((a - 32) * 5/9);
+//check if input is numbrr
+if(isNaN(b)){
+// if not number alert
+	alert("Pease put in a number.");
+}else{
+// If is a number give answer 
+alert('Your temp in celisus is ' + b );
+
+
+}
